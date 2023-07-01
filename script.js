a46d5cbaabd53e63a3b2ab8409b9cbcd4068eccd
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-07-01 13:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input",function(){
+	let text = evaluatedText.value;
+	letterCount.innerText = text.length;
+})
